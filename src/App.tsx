92458ca88c7c4cc3ed182b7d6a7e53b45994c702
KJONDIVE <galkin.anton2000@gmail.com>
2024-10-28 10:45:30 +0500

    feat: App:+ PaperProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigation from './router/Root/RootNavigation';
+import { PaperProvider } from 'react-native-paper';
 
 const App = (): JSX.Element => {
   return (
-    <NavigationContainer>
-      <RootNavigation />
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
